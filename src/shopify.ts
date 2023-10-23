b583d4e252df1e27a70fce9ec143afc747f9e3b1
Biplob Sutradhar <biplobsd11@gmail.com>
2023-10-23 13:57:03 +0000

    Added more comments

--- src/shopify.ts
+++ src/shopify.ts
@@ -4,18 +4,18 @@ import { shopifyApi, ApiVersion } from "@shopify/shopify-api";
 import z from "zod";
 
 // Initialize a TriggerClient
-const client = new TriggerClient({
-  id: "api-reference",
-});
+const client = new TriggerClient({ id: "api-reference" });
 
 // Create a Shopify custom app: https://shopify.dev/tutorials/build-a-shopify-app-with-node-and-react
 // Shopify SDK: https://github.com/Shopify/shopify-api-js
+// Get API credentials by following this tutorial: https://youtu.be/Tn0DzYnRPVQ
+// https://admin.shopify.com/store/<STORE_ID>/settings/apps/development/<APP_ID>/api_credentials
 // Initialize the Shopify API client
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
-  scopes: ["write_products"],
-  hostName: process.env.SHOPIFY_HOSTNAME!,
+  scopes: ["write_products"], // The scopes required for the app can be found in the Shopify Admin Apps page
+  hostName: process.env.SHOPIFY_HOSTNAME!, // Ex: <STORE_ID>.myshopify.com
   apiVersion: ApiVersion.October23,
   isEmbeddedApp: false,
 });
@@ -44,7 +44,7 @@ client.defineJob({
           process.env.SHOPIFY_HOSTNAME!
         );
 
-        // Set the admin access token for the Shopify session
+        // The access token can be found in the Shopify Admin Apps page
         shopifySession.accessToken = process.env.SHOPIFY_ADMIN_ACCESS_TOKEN;
 
         // Initialize a GraphQL Admin Client
