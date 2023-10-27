418ae39dd9603ccc5d8714004423802fa63b46ec
Sudhanshu Pandey <sp6370@nyu.edu>
2023-10-27 07:08:32 -0400

    feat: updated aws lambda example

--- src/aws.ts
+++ src/aws.ts
@@ -1,10 +1,20 @@
 import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
 import { InvokeCommand, LambdaClient, LogType } from "@aws-sdk/client-lambda";
+import { fromEnv } from "@aws-sdk/credential-providers";
 import z from "zod";
 
-// Create Trigger and Lambda clients
 const client = new TriggerClient({ id: "api-reference" });
-const lambdaClient = new LambdaClient({});
+
+// this reference example assumes that, you have setup AWS account and created a lambda function
+// more at: https://docs.aws.amazon.com/lambda/latest/dg/getting-started.html
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/lambda/
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-client-lambda/Interface/LambdaClientConfig/ 
+// https://github.com/aws/aws-sdk-js-v3/tree/main/packages/credential-providers
+const lambdaClient = new LambdaClient({
+    region: process.env.AWS_REGION,
+    credentials: fromEnv(),
+});
 
 // Define a Trigger job to invoke the Lambda function
 client.defineJob({
@@ -15,33 +25,37 @@ client.defineJob({
 
     trigger: eventTrigger({
         name: "aws",
+
+        // lambda function computes area of an rectangle, as such payload object contains length and width
         schema: z.object({
             funcName: z.string(),
-            payloadData: z.string(),
+            payloadObject: z.object({ length: z.number(), width: z.number() }),
         }),
     }),
 
     run: async (payload, io, ctx) => {
         //wrap an SDK call in io.runTask so it's resumable and displays in logs
         const result = await io.runTask(
-            "Invoke aws lambda function",
+            "Invoke lambda",
             async () => {
-                // Invoke the Lambda function with InvokeCommand using AWS Lambda SDK
-
+                // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_lambda_code_examples.html
+                // this is the regular AWS SDK
                 const command = new InvokeCommand({
                     FunctionName: payload.funcName,
-                    Payload: JSON.stringify(payload.payloadData),
+                    Payload: JSON.stringify(payload.payloadObject),
                     LogType: LogType.Tail,
                 });
 
                 const { Payload, LogResult } = await lambdaClient.send(command);
 
                 const result = Buffer.from(Payload).toString();
-                // const logs = Buffer.from(LogResult, "base64").toString();
+                const logs = Buffer.from(LogResult, "base64").toString();
 
-                return result;
-                // return { logs, result };
-            }
+                // returns computed aread and assosiated logs as task output
+                return { result, logs };
+            },
+            //you can add metadata to the task to improve the display in the logs
+            { name: "Invoke Lambda", icon: "aws" }
         );
     },
 });
