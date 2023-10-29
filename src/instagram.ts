15a2b48aaa76b0bb7948d4b32dcecb8d43503a26
Nabin Bhatt <83168881+nabinbhatt@users.noreply.github.com>
2023-10-29 19:41:25 +0000

    add some comments and update example .env file

--- src/instagram.ts
+++ src/instagram.ts
@@ -3,12 +3,14 @@ import z from "zod";
 
 const client = new TriggerClient({ id: "api-reference" });
 
-// Create a access token at developers.facebook.com
+// You need professional instagram account that is connected with your facebook business account 
+// Get started from here: https://developers.facebook.com/docs/instagram-api/getting-started
+// For permanent token you have to create system user: https://business.facebook.com/settings/system-users
+// You can find your instagram user id from here: https://business.facebook.com/settings/instagram-account-v2s
 const token = {
   userId: process.env.INSTAGRAM_USER_ID!,
   accessToken: process.env.FACEBOOK_ACCESS_TOKEN!,
 }
-
 const endpointURLCreate = `https://graph.facebook.com/v18.0/${token.userId}/media`;
 const endpointURLPublish = `https://graph.facebook.com/v18.0/${token.userId}/media_publish`;
 
@@ -24,11 +26,11 @@ client.defineJob({
     }),
   }),
   run: async(payload, io, ctx) => {
-
     // wrap the SDK call in io.runTask for resumability and log display
     await io.runTask(
       "Post on Instagram",
       async () => {
+        // Publishing a single media post is a two step process,
         // Step 1: Create container
         const createContainerUrl = `${endpointURLCreate}?image_url=${payload.imageUrl}&caption=${encodeURIComponent(payload.caption)}&access_token=${token.accessToken}`;
 
@@ -54,8 +56,6 @@ client.defineJob({
     );
   },
 });
-
-
 
 // These lines can be removed if you don't want to use express
 import { createExpressServer } from "@trigger.dev/express";
