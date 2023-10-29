852906186f9c8d84ea825660a24f9c1638b96d05
Suvekshya <khanalsagar83@gmail.com>
2023-10-29 15:54:12 +0545

    refactor: updated example to use a different function

--- src/lemon-squeezy.ts
+++ src/lemon-squeezy.ts
@@ -1,7 +1,11 @@
-import LemonSqueezy from "@lemonsqueezy/lemonsqueezy.js/dist/index.cjs";
+import z from "zod";
 import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
 
-// 
+// The import path here has been updated because of conflict between commonjs and ESmodule
+// when using in proper ESmodule setup you won't have to do this
+import LemonSqueezy from "@lemonsqueezy/lemonsqueezy.js/dist/index.cjs";
+
+//
 const client = new TriggerClient({ id: "api-reference" });
 
 // Get API key from https://docs.lemonsqueezy.com/api#authentication
@@ -9,23 +13,40 @@ const ls = new LemonSqueezy(process.env.LEMONSQUEEZY_API_KEY!);
 
 // Using official SDK; https://github.com/lmsqueezy/lemonsqueezy.js
 client.defineJob({
-  id: "get-lemon-squeezy-current-user",
-  name: "Get Lemon Squeezy current user",
+  id: "get-lemon-squeezy-store-details",
+  name: "Get Lemon Squeezy store details",
   version: "1.0.0",
   trigger: eventTrigger({
-    name: "get-lemon-squeezy-current-user",
+    name: "get-lemon-squeezy-store-details",
+    schema: z.object({
+      id: z.string(),
+      include: z
+        .array(
+          z.enum([
+            "products",
+            "discounts",
+            "license-keys",
+            "subscriptions",
+            "webhooks",
+          ])
+        )
+        .optional(),
+    }),
   }),
   run: async (payload, io) => {
     await io.runTask(
-      "Get Lemon Squeezy current user",
+      "Get Lemon Squeezy store details",
       async () => {
-        const user = await ls.getUser();
+        const store = await ls.getStore({
+          id: payload.id,
+          include: payload.include,
+        });
         // The return value has to be JSON serialiazable as it is stored in the databse.
         // Thus stringifying and then parsing it in order to make sure that it is always a serializable JSON
-        return JSON.parse(JSON.stringify(user));
+        return JSON.parse(JSON.stringify(store));
       },
       //you can add metadata to the task to improve the display in the logs
-      { name: "Get Lemon Squeezy current user", icon: "lemonSqueezy" }
+      { name: "Get Lemon Squeezy store details", icon: "lemonSqueezy" }
     );
   },
 });
