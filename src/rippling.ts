fb7854f16bceb51d671838621c950c689444fd92
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-10-29 19:22:13 +0530

    Update rippling.ts

--- src/rippling.ts
+++ src/rippling.ts
@@ -1,12 +1,19 @@
+// Import necessary packages and libraries
 import { eventTrigger } from "@trigger.dev/sdk";
 import z from "zod";
 
+// Define the fetch function signature for type safety
 let fetch: ((url: URL | RequestInfo, init?: RequestInit | undefined) => Promise<Response>) | ((arg0: string, arg1: { method: string; headers: { Authorization: string; }; }) => any);
+
+// Use dynamic import to fetch the node-fetch library
 import('node-fetch').then(fetchedModule => {
     fetch = fetchedModule.default || fetchedModule;
 });
 
+// Set up TriggerClient with the SDK
 const TriggerClient = require("@trigger.dev/sdk").TriggerClient;
+
+// Define the payload schema for the rippling event
 const ripplingPayloadSchema = z.object({
     eventId: z.string(),
     eventData: z.object({
@@ -16,8 +23,10 @@ const ripplingPayloadSchema = z.object({
     })
 });
 
+// Initialize the Trigger client with an ID
 const client = new TriggerClient({ id: "api-reference" });
 
+// Define the job with its details, triggers, and run function
 client.defineJob({
     id: "rippling-api-job",
     name: "Rippling API Job",
@@ -27,10 +36,13 @@ client.defineJob({
         schema: ripplingPayloadSchema
     }),
     run: async () => {
+        // If fetch is not initialized, try initializing it before using
         if (!fetch) {
-            throw new Error("node-fetch has not been initialized yet.");
+            const fetchedModule = await import('node-fetch');
+            fetch = fetchedModule.default || fetchedModule;
         }
 
+        // Make a request to the Rippling API
         const response = await fetch('RIPPLING_API_ENDPOINT', {
             method: 'GET',
             headers: {
@@ -38,11 +50,12 @@ client.defineJob({
             }
         });
 
+        // Parse the response to JSON and return the data
         const data = await response.json();
         return data;
     }
 });
 
-// For express integration
+// Set up express server for integration with the Trigger client
 const { createExpressServer } = require("@trigger.dev/express");
 createExpressServer(client);
