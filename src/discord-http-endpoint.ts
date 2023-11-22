cf868210d062f79af828e2ba08db02059f7a3a76
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-11-22 19:24:05 +0530

    commit

--- src/discord-http-endpoint.ts
+++ src/discord-http-endpoint.ts
@@ -5,23 +5,45 @@ import { TriggerClient } from "@trigger.dev/sdk";
 const client = new TriggerClient({ id: "api-reference" });
 // end-hide-code
 
+const verifyRequestSignature = async (request: Request): Promise<any[]> => {
+    const body = await request.text();
+    const jsonBody = JSON.parse(body)
+    const signature = request.headers.get('x-signature-ed25519')
+    const timestamp = request.headers.get('x-signature-timestamp')
+    const discordKey = process.env.DISCORD_APPLICATION_KEY
+    if (!discordKey || !signature || !timestamp) return [false, jsonBody]
+    return [nacl.sign.detached.verify(
+        Buffer.from(timestamp + body),
+        Buffer.from(signature, "hex"),
+        Buffer.from(discordKey, "hex")
+    ), jsonBody];
+}
+
 const discord = client.defineHttpEndpoint({
     id: "discord",
     source: "discord.com",
     icon: "discord",
+    // only needed for strange APIs like Discord which don't setup the webhook until you pass the test
+    respondWith: {
+        // don't trigger runs if they match this filter
+        skipTriggeringRuns: true,
+        filter: {
+            method: ["POST"],
+        },
+        handler: async (request) => {
+            const success = await verifyRequestSignature(request)
+            console.log('[Handler]:')
+            console.log(success[0])
+            if (!success[0]) return new Response("Unauthorized", { status: 401 });
+            const { type } = success[1]
+            return new Response(JSON.stringify({ type }), { headers: { 'Content-Type': 'application/json' } });
+        },
+    },
     verify: async (request) => {
-        const body = await request.text();
-        const signature = request.headers.get('x-signature-ed25519')
-        const timestamp = request.headers.get('x-signature-timestamp')
-        const discordKey = process.env.DISCORD_APPLICATION_KEY
-        if (!discordKey) return { success: false, reason: 'Missing discord public key' }
-        if (!signature || !timestamp) return { success: false, reason: 'Missing discord headers' }
-        const success = nacl.sign.detached.verify(
-            Buffer.from(timestamp + body),
-            Buffer.from(signature, "hex"),
-            Buffer.from(discordKey, "hex")
-        );
-        if (success) return { success };
+        const success = await verifyRequestSignature(request)
+        console.log('[Verify]:')
+        console.log(success[0])
+        if (success[0]) return { success: success[0] };
         return { success: false, reason: "Failed ed25519 verification" };
     },
 });
