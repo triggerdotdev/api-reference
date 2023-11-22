5d324546d4024aa42d0f5fde5c1c6b7a2bec8fbb
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-11-22 21:28:32 +0530

    webhook

---
+++ src/hugging-face-http-endpoint.ts
@@ -0,0 +1,34 @@
+import { TriggerClient } from "@trigger.dev/sdk";
+import { createHash } from "crypto";
+
+// hide-code
+const client = new TriggerClient({ id: "api-reference" });
+// end-hide-code
+
+const hubspot = client.defineHttpEndpoint({
+  id: "hugging-face",
+  source: "huggingface.co",
+  icon: "hugging-face",
+  verify: async (request) => {
+    if (request.headers.get("x-webhook-secret") === process.env.HUGGING_FACE_WEBHOOK_SECRET) return { success: true };
+    return { success: false, reason: "Webhook Secret Match Failed" };
+  },
+});
+
+client.defineJob({
+  id: "http-hugging-face",
+  name: "HTTP Hugging Face",
+  version: "1.0.0",
+  enabled: true,
+  trigger: hubspot.onRequest(),
+  run: async (request, io, ctx) => {
+    const body = await request.json();
+    await io.logger.info(`Body`, body);
+  },
+});
+
+// hide-code
+// These lines can be removed if you don't want to use express
+import { createExpressServer } from "@trigger.dev/express";
+createExpressServer(client);
+// end-hide-code
