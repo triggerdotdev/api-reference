88443644b591c9abc0c3e06f909109040e99e66d
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2023-12-04 21:19:41 +0530

    update instructions

--- src/google-sheets-http-endpoint.ts
+++ src/google-sheets-http-endpoint.ts
@@ -7,12 +7,42 @@ const client = new TriggerClient({ id: "api-reference" });
 // Apps Script Docs: https://developers.google.com/apps-script/reference
 // Triggers Docs: https://developers.google.com/apps-script/guides/triggers
 
-// Open your Google Sheets document
-// Go to Extensions > Apps Script
-// Get endpoint URL and secret from `trigger.dev` dashboard
-// create a new script with function that makes a POST request to the endpoint URL
-// with x-webhook-secret header and the event data as the body
-// Go to Triggers > Add Trigger > Select "select event type" > Save
+// Go to https://docs.google.com/spreadsheets
+// Open a spreadsheet or create a new one
+// Click on Extensions > Apps Script
+
+// Add the following code to the script editor
+// function pushNotification(e) {
+//   // get endpoint url and secret key from trigger.dev dashboard
+//   var url = ''
+//   var secretKey = ''
+
+//   var payloadData = {
+//     'message': 'Google Sheets event',
+//     'details': e // This contains information about the edit event
+//   };
+
+//   var payload = JSON.stringify(payloadData);
+
+//   var options = {
+//     'method': 'post',
+//     'contentType': 'application/json',
+//     'headers': {
+//       'x-webhook-secret': secretKey
+//     },
+//     'payload': payload
+//   };
+
+//   UrlFetchApp.fetch(url, options);
+// }
+
+// Save the script and click on the clock icon (Triggers) in the sidebar to open the Triggers page
+// Click on Add Trigger, which will open a dialog box
+// choose pushNotification as the function to run
+// select event type, e.g. On open, On edit, On change etc.
+// Click Save
+
+// Set the GOOGLE_SHEETS_WEBHOOK_SECRET (Secret) in the .env file.
 
 // Create an HTTP Endpoint, with the Google Sheets details
 const sheets = client.defineHttpEndpoint({
